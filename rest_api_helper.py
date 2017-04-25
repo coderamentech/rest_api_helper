e1bf4bbf2ab10b07cb9fe7f3c8a57f6d1d139a2f
Ron Michael <ron.michael.khu@gmail.com>
2017-04-25 21:09:22 +0800

    Add decorator for CORS

--- rest_api_helper.py
+++ rest_api_helper.py
@@ -23,7 +23,58 @@
 from flask import Flask, Response, request
 import json, os, pprint, uuid, inspect, threading, atexit
 
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+
+
 class Util:
+
+    @staticmethod
+    def crossdomain(origin=None, methods=None, headers=None,
+                    max_age=21600, attach_to_all=True,
+                    automatic_options=True):
+        """
+        from: http://flask.pocoo.org/snippets/56/ 
+        """
+        if methods is not None:
+            methods = ', '.join(sorted(x.upper() for x in methods))
+        if headers is not None and not isinstance(headers, basestring):
+            headers = ', '.join(x.upper() for x in headers)
+        if not isinstance(origin, basestring):
+            origin = ', '.join(origin)
+        if isinstance(max_age, timedelta):
+            max_age = max_age.total_seconds()
+
+        def get_methods():
+            if methods is not None:
+                return methods
+
+            options_resp = current_app.make_default_options_response()
+            return options_resp.headers['allow']
+
+        def decorator(f):
+            def wrapped_function(*args, **kwargs):
+                if automatic_options and request.method == 'OPTIONS':
+                    resp = current_app.make_default_options_response()
+                else:
+                    resp = make_response(f(*args, **kwargs))
+                if not attach_to_all and request.method != 'OPTIONS':
+                    return resp
+
+                h = resp.headers
+
+                h['Access-Control-Allow-Origin'] = origin
+                h['Access-Control-Allow-Methods'] = get_methods()
+                h['Access-Control-Max-Age'] = str(max_age)
+                if headers is not None:
+                    h['Access-Control-Allow-Headers'] = headers
+                return resp
+
+            f.provide_automatic_options = False
+            return update_wrapper(wrapped_function, f)
+        return decorator
+
     """Contains assorted utility or helper methods for this module
     """
 
@@ -350,7 +401,6 @@ class LazyManager:
 
             status = 200
             content = '[' + ','.join(contents) + ']'
-        #pprint.pprint(entry)
 
         resp = Response(response=content,
                        status=status,
@@ -374,7 +424,7 @@ class LazyManager:
         if not LazyManager.collection_configs.has_key(collection):
             return Response(status = 404)
 
-        if request.method == 'GET':
+        if request.method == 'GET' or request.method == 'OPTIONS' :
             return self.get_data_entries(request, collection, entry_id)
 
         if request.method == 'DELETE':
